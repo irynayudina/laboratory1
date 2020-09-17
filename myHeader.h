83b16cb3edc6c45521aea44eee49d1d0eb2a556b
Iryna Yudina <yudinaira4444@gmail.com>
2020-09-17 18:26:15 +0300

    final

--- myHeader.h
+++ myHeader.h
@@ -5,15 +5,3 @@ string Concatination(string s1, string s2);
 int Addition(int x, int y);
 int Multiplication(int x, int y);
 bool check_number(std::string str);
-//class Animal {
-////public:
-////	int amountOfLegs = 4;
-////	void print() {
-////		std::cout << "sound" << std::endl;
-////	}
-//};
-//class Cat : Animal {
-////public: void print() {
-////	std::cout << "meow" << std::endl;
-////}
-//};
